b67f79c3423c46d435257cc3c11d277bac82adc5
Antonis Maronikolakis <antmarakis@delta.cis.uni-muenchen.de>
2021-12-02 14:32:53 +0100

    default

--- dynamic_programming/optimal_matrix_path.py
+++ dynamic_programming/optimal_matrix_path.py
@@ -6,28 +6,27 @@ import numpy as np
 matrix = [[5, 3, 10, 17, 1],
           [4, 2, 9, 8, 5],
           [11, 12, 3, 9, 6],
-          [1, 3, 4, 2, 10],
-          [7, 11, 13, 7, 3]]
+          [1, 3, 4, 2, 10]]
 
 matrix = np.matrix(matrix)
-print(matrix)
 
 def optimal_path(n, m):
-    solutions = np.matrix([[-1 for i in range(n)] for j in range(m)])
-    path = [[(-1, -1) for i in range(n)] for j in range(m)]
+    solutions = np.matrix([[-1 for i in range(m)] for j in range(n)])
+    path = [[(-1, -1) for i in range(m)] for j in range(n)]
     
     solutions[0, 0], path[0][0] = matrix[0, 0], (0, 0)
     
-    # left column
-    for j in range(1, m):
-        solutions[0, j] = solutions[0, j-1] + matrix[0, j]
-        path[0][j] = (0, j-1)
-    
-    # top row
+    # initialize left column
     for i in range(1, n):
         solutions[i, 0] = solutions[i-1, 0] + matrix[i, 0]
         path[i][0] = (i-1, 0)
     
+    # initialize top row
+    for j in range(1, m):
+        solutions[0, j] = solutions[0, j-1] + matrix[0, j]
+        path[0][j] = (0, j-1)
+    
+    # build rest
     for i in range(1, n):
         for j in range(1, m):
             if solutions[i-1, j] < solutions[i, j-1]:
@@ -48,4 +47,4 @@ def optimal_path(n, m):
         print(p)
         if p == (0, 0): break
 
-optimal_path(5, 5)
+optimal_path(matrix.shape[0], matrix.shape[1])
